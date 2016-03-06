056f08836a8307e31ba509d13b2cd39596a347fd
dstrodtman <douglasstrodtman@Douglass-MacBook-Air.local>
2016-03-06 09:58:24 -0800

    Added Stimuli IDs
    Added stimuli IDs, active target highlight, rudimentary button to
    proceed to next task. Reworked a few previous calls into functions.

--- idTargets.m
+++ idTargets.m
@@ -1,9 +1,11 @@
+function tarId = idTargets(strLength)
+
 %Make targets unique entities for hover highlight, locking, and data
 %tracking. Will later communicate with stimuli info.
 
 % Create cell array for cards
-target = cell(1, tarNum);
+target = cell(1, strLength);
 
-for ii = 1:tarNum
+for ii = 1:strLength
     target{ii} = finalTarRect(1:4, ii)'; % Define targets
 end
--- tarRect.m
+++ tarRect.m
@@ -1,12 +1,12 @@
+function [tarDims, target] = tarRect(strLength,screenXpixels,yCenter)
 
-%This program is just to automatically test visual spacing of stimuli on
-%screens of different sizes. These numbers will be fixed once final
-%hardware is decided so as to prevent potential processing delays. I am
-%aware of code redundancies, but wanted to make it easy to change constants
-%to determine final layout.
+%This program automatically sets the spacing of targets for each stimuli
+%string and adjusts for their length on any size presentation screen.
+%Currently set to have card stimuli center on the upper quartile of the
+%screen.
 
-% Creates random number of target rects, testing functionality
-tarNum = randi([3,7]);
+% Set target number to string length
+tarNum = strLength;
 
 % Set rectangle dimensions
 rectX = 150;
@@ -18,22 +18,32 @@ rectSpace = 25;
 % Build target rectangle matrix and set height
 rectMat = repmat([0; 0; 0; rectY], 1, tarNum);
 
-% Set X
+% Set horizontal location
 for ii = (1:tarNum)
     rectMat([1, 3],ii) = [0, rectX] + (rectX + rectSpace) * (ii-1);
 end
 
-% Build matrix to center rects in top half of screen.
+% Build matrix to center targets in top half of screen.
 xshift = (screenXpixels - rectMat(1,tarNum)) / 2 - rectX / 2;
 yshift = yCenter - .5*yCenter - .5*rectY;
 matShift = repmat([xshift; yshift], [2, tarNum]);
 
-% Adjust rect coordinates to top half of screen.
-finalTarRect = rectMat + matShift;
+% Adjust target coordinates to top half of screen, output.
+tarDims = rectMat + matShift;
+
+%Make targets unique entities for hover highlight, locking, and data
+%tracking. Will later communicate with stimuli info.
+
+% Create cell array for cards
+target = cell(1, strLength);
+
+for ii = 1:strLength
+    target{ii} = tarDims(1:4, ii)'; % Define targets
+end
 
 % Set targets to draw
-Screen('FillRect', window, 1, finalTarRect);
-Screen('FrameRect', window, .5, finalTarRect, 5);
+% Screen('FillRect', window, 1, finalTarRect);
+% Screen('FrameRect', window, .5, finalTarRect, 5);
 
 %Assign each target an identity
-idTargets;
+% idTargets;
